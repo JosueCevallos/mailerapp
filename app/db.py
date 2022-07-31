5af93bb4a8c24c68e57c2dd0df7d605599265358
josue cevallos gonzalez <josuecevallos.g@gmail.com>
2022-07-31 19:14:29 +0200

    Creando funcion get_db

---
+++ app/db.py
@@ -0,0 +1,30 @@
+import mysql.connector
+import click 
+from flask import current_app, g
+from flask.cli import with_appcontext
+from .schema import instructions
+
+
+"""------------------DOCUMENTATION---------------
+* current_app: can be used to access data about the running application,
+including the configuration. This is useful for both developers using 
+the framework and ones building extensions for Flask.
+
+* g: is an object for storing data during the application context of a 
+running Flask web app.
+
+* with_appcontext is a Flask decorator in the flask.cli module that wraps 
+a callback to guarantee it will be called with a script's application context. 
+Any callbacks registered with app.cli are wrapped with this function by default.
+
+* schema is a libray that allows to create data structures (tables, columns, relationships, ...)"""
+
+def get_db():
+
+    if 'db' not in g:
+        g.db = mysql.connector.connect(
+            host = current_app.config["DATABASE_HOST"],
+            user = current_app.config["DATABASE_USER"],
+            password = current_app.config["DATABASE_PASSWORD"],
+            database = current_app.config["FLASK_DATABASE"]
+        )
