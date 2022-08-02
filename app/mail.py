7382754d6b29a5e90fbee062cacccdfecd2e7ea3
josue cevallos gonzalez <josuecevallos.g@gmail.com>
2022-08-03 01:06:17 +0200

    elimino linea 'import crypt' del archivo. Modulo no soportado en windows

--- app/mail.py
+++ app/mail.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import (Blueprint)
 
 bp = Blueprint('mail',__name__, url_prefix="/") #nombre del blueprint, nombre de la aplicacion, ruta por defecto al iniciar la app
