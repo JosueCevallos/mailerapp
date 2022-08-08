1f276dbd9e6803fc2f367afb27b5f60219ce9840
josue cevallos gonzalez <josuecevallos.g@gmail.com>
2022-08-08 18:23:00 +0200

    añadir formulario para agregar texto y buscar

--- app/mail.py
+++ app/mail.py
@@ -1,8 +1,14 @@
-from flask import (Blueprint)
+from flask import (Blueprint, render_template)
+from app.db import get_db
 
 bp = Blueprint('mail',__name__, url_prefix="/") #nombre del blueprint, nombre de la aplicacion, ruta por defecto al iniciar la app
 
 @bp.route('/', methods=['GET'])
 
 def index():
-    return 'prueba'
+    db,c = get_db()
+    c.execute("SELECT * FROM email")
+    mails = c.fetchall()
+
+    
+    return render_template('mails/index.html', mails=mails)
