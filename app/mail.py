1ae5c9ea798fb5274c83393f233a92052a829050
josue cevallos gonzalez <josuecevallos.g@gmail.com>
2022-08-08 19:44:51 +0200

    modifico la funcion create para obtener los valores ingresados en el formulario y creo un listado con errores en caso de añadir un campo vacio

--- app/mail.py
+++ app/mail.py
@@ -1,8 +1,39 @@
-from flask import (Blueprint)
+from distutils.log import error
+import email
+from flask import (Blueprint, flash, render_template, request)
+from app.db import get_db
 
 bp = Blueprint('mail',__name__, url_prefix="/") #nombre del blueprint, nombre de la aplicacion, ruta por defecto al iniciar la app
 
 @bp.route('/', methods=['GET'])
 
 def index():
-    return 'prueba'
+    db,c = get_db()
+    c.execute("SELECT * FROM email")
+    mails = c.fetchall()
+
+    return render_template('mails/index.html', mails=mails)
+
+@bp.route('/create', methods=['GET','POST'])
+def create():
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        subject = request.form.get("subject")
+        content = request.form.get("content")
+        errors = []
+
+        if not email:
+            errors.append("El email es obligatorio")
+        if not subject:
+            errors.append("El asunto es obligatorio")
+        if not content:
+            errors.append("El contenido es obligatorio")
+
+        if len(errors) == 0:
+            pass
+        else:
+            for error in errors:
+                flash(error)
+
+    return render_template('mails/create.html')
