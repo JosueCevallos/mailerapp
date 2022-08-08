7bc93abe7bf51e68c82ae7a865fd0dffb63263bc
josue cevallos gonzalez <josuecevallos.g@gmail.com>
2022-08-08 18:26:12 +0200

    "Agrego instruccion para obtener los registros de la base de datos"
    This reverts commit 1f276dbd9e6803fc2f367afb27b5f60219ce9840.

--- app/mail.py
+++ app/mail.py
@@ -1,14 +1,8 @@
-from flask import (Blueprint, render_template)
-from app.db import get_db
+from flask import (Blueprint)
 
 bp = Blueprint('mail',__name__, url_prefix="/") #nombre del blueprint, nombre de la aplicacion, ruta por defecto al iniciar la app
 
 @bp.route('/', methods=['GET'])
 
 def index():
-    db,c = get_db()
-    c.execute("SELECT * FROM email")
-    mails = c.fetchall()
-
-    
-    return render_template('mails/index.html', mails=mails)
+    return 'prueba'
